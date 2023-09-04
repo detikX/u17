4f65efb93f0f8534e0f5dfbd8450beccd96021e6
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-09-04 14:17:51 +0700

    frf

--- js/app.js
+++ js/app.js
@@ -3,3 +3,5 @@ $(document).ready(function() {
 })
 
 $(".bawah li").attr({"data-aos":"fade-in", "data-aos-delay":"500", "data-aos-duration":"1000"})
+
+$(".card_anim .imgs").attr({"data-aos":"fade-in", "data-aos-delay":"500", "data-aos-duration":"1000"})

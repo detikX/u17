23ca72bbeddf474781873b62cb0a904b1841857a
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-11-01 16:01:09 +0700

    ded

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
 $(document).ready(function () {
-  AOS.init()
+  AOS.init();
 })
+// if (event.cancelable) event.preventDefault();
+
+
+$('.daysx').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
 
 $(".bawah li").attr({ "data-aos": "fade-in", "data-aos-delay": "300", "data-aos-duration": "1000" })
 
